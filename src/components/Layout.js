d8152b2b63f0dccd8b3edbe95956997bb0683cc7
j10sanders <jonsandersss@gmail.com>
2019-02-13 22:18:54 -0500

    start using a toggle

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,9 +3,9 @@ import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from "gatsby"
 import { loadState, saveState } from '../utils/helpers'
 import Navbar from '../components/Navbar'
+import Switch from 'react-switch'
+import moon from '../img/moon.svg'
 import './all.sass'
-
-const MyContext = React.createContext(); 
 
 const TemplateWrapper = class extends React.Component {
   constructor() {
@@ -19,6 +19,7 @@ const TemplateWrapper = class extends React.Component {
 
   toggleLightMode() {
     const lightMode = !this.state.lightMode
+    console.log(lightMode)
     saveState('lightMode', lightMode)
     this.setState({ lightMode })
   }
@@ -51,11 +52,11 @@ const TemplateWrapper = class extends React.Component {
               <html lang="en" />
               <title>{data.site.siteMetadata.title}</title>
               <meta name="description" content={data.site.siteMetadata.description} />
-              
+
               <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png" />
               <link rel="icon" type="image/png" href="/img/favicon-32x32.png" sizes="32x32" />
               <link rel="icon" type="image/png" href="/img/favicon-16x16.png" sizes="16x16" />
-      
+
               <link rel="mask-icon" href="/img/safari-pinned-tab.svg" color="#ff4400" />
               <meta name="theme-color" content="#fff" />
 
@@ -64,21 +65,31 @@ const TemplateWrapper = class extends React.Component {
               <meta property="og:url" content="/" />
               <meta property="og:image" content="/img/og-image.jpg" />
             </Helmet>
-            <Navbar lightMode={lightMode}/>
+            <Navbar lightMode={lightMode} />
             <div
-             
+
             >
-              <button
-                style={{
-                  color: '#ffa7c4',
-                  float: 'right',
-                  background: lightMode ? '#212121' : '#ffffff',
-                }}
-                onClick={this.toggleLightMode}
-              >
-            { lightMode ? 'Dark' : 'Light'}
-              </button>
-             </div>
+              <Switch
+                id="lightMode"
+                onColor="#D3D3D3"
+                offColor="#303030"
+                checkedIcon={<svg
+                  height="100%"
+                  width="100%"
+                  viewBox="-2 -5 17 21"
+                  style={{ position: "absolute", top: 0 }}
+                >
+                  <path
+                    d="M11.264 0L5.26 6.004 2.103 2.847 0 4.95l5.26 5.26 8.108-8.107L11.264 0"
+                    fill="#fff"
+                    fillRule="evenodd"
+                  />
+                </svg>}
+                checked={!this.state.lightMode}
+                onChange={this.toggleLightMode}
+
+              />
+            </div>
             <div>{this.props.children}</div>
           </div>
         )}
