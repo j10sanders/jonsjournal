d5e0aeb3b5560c5b66ddb7d864d5ebfb36d9ed26
j10sanders <jonsandersss@gmail.com>
2019-03-02 14:40:05 -0500

    remove data

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,7 +26,7 @@ export default class IndexPage extends React.Component {
     const { edges: posts } = data.allMarkdownRemark
 
     return (
-      <Layout slug={data.markdownRemark.fields.slug}>
+      <Layout>
         <div className="content">
           <p className="has-text-weight-bold is-size-2">Latest Journalings</p>
         </div>
