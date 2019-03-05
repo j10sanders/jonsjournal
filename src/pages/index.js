2332eec703d2ca763ed4f8db5557125b3f7c09d2
Jonathan Sanders <jonsandersss@gmail.com>
2019-03-05 10:20:02 -0500

    cleanup again

--- src/pages/index.js
+++ src/pages/index.js
@@ -71,7 +71,7 @@ export const pageQuery = graphql`
     ) {
       edges {
         node {
-          excerpt(pruneLength: 400)
+          excerpt(pruneLength: 200)
           id
           fields {
             slug
